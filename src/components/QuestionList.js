0595e4a92b2dc81cfbf3475e709b8a0fe893cf81
lucasfarmwald <farmwaldlucas@gmail.com>
2022-02-14 19:17:15 -0600

    added useEffect for delete button

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([])
+
+useEffect(() => {
+  fetch('http://localhost:4000/questions')
+  .then((r) => r.json())
+  .then((questions) => {
+    setQuestions(questions);
+  });
+}, []);
+
+function handleDeleteClick(id) {
+  fetch(`http://localhost:4000/questions${id}`,{
+    method: "DELETE"
+  }
+  )
+  .then((r) => r.json())
+}
+
+const questionItems = questions.map((q) => (
+  <QuestionItem
+  key={q.id}
+  question={q}
+  onDeleteClick={handleDeleteClick}
+  />
+))
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
